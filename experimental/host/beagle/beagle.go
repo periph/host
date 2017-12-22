periph/host

commit 649788fd5182ef30bad22746839198612175bfe0

Add experimental/host packages am335x, beagle and pruicss
Refer to issues #199, #200 and #201.

experimental/host/beagle/beagle.go
@@ -1,0 +1,38 @@
+// Copyright 2017 The Periph Authors. All rights reserved.
+// Use of this source code is governed under the Apache License, Version 2.0
+// that can be found in the LICENSE file.
+
+package beagle
+
+import (
+	"errors"
+
+	"periph.io/x/periph"
+)
+
+// driver implements periph.Driver.
+type driver struct {
+}
+
+func (d *driver) String() string {
+	return "beagle"
+}
+
+func (d *driver) Prerequisites() []string {
+	return nil
+}
+
+func (d *driver) Init() (bool, error) {
+	if !Present() {
+		return false, errors.New("BeagleBoard/BeagleBone board not detected")
+	}
+	return true, nil
+}
+
+func init() {
+	if isArm {
+		periph.MustRegister(&driver{})
+	}
+}
+
+var _ periph.Driver = &driver{}

experimental/host/am335x/am335x.go
@@ -1,0 +1,46 @@
+// Copyright 2017 The Periph Authors. All rights reserved.
+// Use of this source code is governed under the Apache License, Version 2.0
+// that can be found in the LICENSE file.
+
+package am335x
+
+import (
+	"errors"
+
+	"periph.io/x/periph"
+)
+
+// Present returns true if an am335x processor is detected.
+//
+// TODO(maruel): Implement.
+func Present() bool {
+	if isArm {
+		return false
+	}
+	return false
+}
+
+// driver implements periph.Driver.
+type driver struct {
+}
+
+func (d *driver) String() string {
+	return "am335x"
+}
+
+func (d *driver) Prerequisites() []string {
+	return nil
+}
+
+func (d *driver) Init() (bool, error) {
+	if !Present() {
+		return false, errors.New("am335x CPU not detected")
+	}
+	return true, nil
+}
+
+func init() {
+	if isArm {
+		periph.MustRegister(&driver{})
+	}
+}

experimental/host/am335x/am335x_arm.go
@@ -1,0 +1,7 @@
+// Copyright 2017 The Periph Authors. All rights reserved.
+// Use of this source code is governed under the Apache License, Version 2.0
+// that can be found in the LICENSE file.
+
+package am335x
+
+const isArm = true

experimental/host/am335x/am335x_other.go
@@ -1,0 +1,9 @@
+// Copyright 2017 The Periph Authors. All rights reserved.
+// Use of this source code is governed under the Apache License, Version 2.0
+// that can be found in the LICENSE file.
+
+// +build !arm
+
+package am335x
+
+const isArm = false

experimental/host/am335x/doc.go
@@ -1,0 +1,23 @@
+// Copyright 2017 The Periph Authors. All rights reserved.
+// Use of this source code is governed under the Apache License, Version 2.0
+// that can be found in the LICENSE file.
+
+// Package am335x exposes functionality for the Texas Instruments Sitara AM335x
+// processor family.
+//
+// This processor family is found on the BeagleBone. PRU-ICSS functionality is
+// implemented in package pruicss.
+//
+// Datasheet
+//
+// Technical Reference Manual
+// https://www.ti.com/lit/ug/spruh73p/spruh73p.pdf
+//
+// Other
+//
+// Marketing page
+// https://www.ti.com/processors/sitara/arm-cortex-a8/am335x/overview.html
+//
+// Family overview
+// https://www.ti.com/lit/ds/symlink/am3359.pdf
+package am335x

experimental/host/beagle/beagle_arm.go
@@ -1,0 +1,7 @@
+// Copyright 2017 The Periph Authors. All rights reserved.
+// Use of this source code is governed under the Apache License, Version 2.0
+// that can be found in the LICENSE file.
+
+package beagle
+
+const isArm = true

experimental/host/beagle/beagle_other.go
@@ -1,0 +1,9 @@
+// Copyright 2017 The Periph Authors. All rights reserved.
+// Use of this source code is governed under the Apache License, Version 2.0
+// that can be found in the LICENSE file.
+
+// +build !arm
+
+package beagle
+
+const isArm = false

experimental/host/beagle/doc.go
@@ -1,0 +1,15 @@
+// Copyright 2017 The Periph Authors. All rights reserved.
+// Use of this source code is governed under the Apache License, Version 2.0
+// that can be found in the LICENSE file.
+
+// Package beagle contains BeagleBoard/BeagleBone board family headers
+// definition.
+//
+// While most boards uses Texas Instruments CPUs, some use CPUs from Octavo
+// Systems.
+//
+// Physical
+//
+// The physical pin out is based on information at
+// https://beagleboard.org/support/bone101/.
+package beagle

commit d646cf2075b50fa53b5e07572c638199383982b7

Fix compile regression in 649788fd5182

experimental/host/beagle/beagle.go
@@ -23,10 +23,7 @@ func (d *driver) Prerequisites() []string {
 }
 
 func (d *driver) Init() (bool, error) {
-	if !Present() {
-		return false, errors.New("BeagleBoard/BeagleBone board not detected")
-	}
-	return true, nil
+	return false, errors.New("BeagleBoard/BeagleBone board not detected")
 }
 
 func init() {